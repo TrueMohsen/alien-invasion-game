c626858f8a2520ce782bdc595c8b8feec239b213
TrueMohsen <66471921+TrueMohsen@users.noreply.github.com>
2020-08-27 01:15:30 +0430

    Add files via upload
    space added from truemohsen

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship():
         self.image=pygame.image.load('images/ship.bmp')
         self.rect=self.image.get_rect()
         self.screen_rect = screen.get_rect()
+        
         self.rect.centerx=self.screen_rect.centerx
         self.rect.bottom=self.screen_rect.bottom
         self.center=float(self.rect.centerx)
