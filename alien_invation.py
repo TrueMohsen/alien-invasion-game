f7cba03dc19baf1a50531fcd6764054da8a03617
TrueMohsen <66471921+TrueMohsen@users.noreply.github.com>
2020-08-30 04:00:15 +0430

    Add files via upload

--- alien_invation.py
+++ alien_invation.py
@@ -2,8 +2,10 @@ from builtins import len
 
 import pygame
 import game_functions as gf
+
 from settings import Settings
 from ship import Ship
+from alien import Alien
 from pygame.sprite import Group
 
 def run_game():
@@ -12,15 +14,14 @@ def run_game():
     screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship=Ship(ai_settings,screen)
+    alien=Alien(ai_settings,screen)
     bullets=Group()
 
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
-
-
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,alien,bullets)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -23,6 +23,8 @@ def check_keydown_events(event,ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullets(ai_settings,screen,ship,bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 
 def check_keyup_events(event,ship):
@@ -34,12 +36,13 @@ def check_keyup_events(event,ship):
 
 
 
-def update_screen(ai_settings,screen,ship, bullets):
+def update_screen(ai_settings,screen,ship, alien,bullets):
     """Updates images on screen and flip to the new screen"""
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
     pygame.display.flip()
 
 def update_bullets(bullets):
